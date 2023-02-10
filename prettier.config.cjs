a10b236375e792f69e5a8a2ccf52a7db174e5546
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-02-10 23:30:45 +0330

    feat(prettier): update prettier config to add import order

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,23 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "all",
+  semi: true,
+  importOrder: [
+    "^@core/(.*)$",
+    "next/",
+    "<THIRD_PARTY_MODULES>",
+    "^@server/(.*)$",
+    "^@mui/(.*)$",
+    "@/components/",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    require.resolve(
+      "prettier-plugin-tailwindcss",
+    ),
+  ],
 };
