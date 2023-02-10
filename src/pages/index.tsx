8609edf0e4991ae79d2f6108e73759192d0131ec
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-02-10 23:18:55 +0330

    feat(mui): install and config MUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,10 @@ const Home: NextPage = () => {
         <meta name="description" content="Mohammad H. Bahrampour Portfolio" />
         <link rel="icon" href="favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="favicon.png" type="image/png" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
 
       <main></main>
