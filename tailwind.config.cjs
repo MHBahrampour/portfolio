c5d3133ce3cf48e1a3c3960dfd272a5dec82a599
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-02-11 20:58:49 +0330

    style(tailwind): set Tailwind breakpoints like MUI breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  important: "#root",
   theme: {
+    screens: {
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
     extend: {},
   },
   corePlugins: {
-    // Remove Tailwind CSS's preflight style so it can use the MUI's preflight instead (CssBaseline).
     preflight: false,
   },
   plugins: [],
