80345b49f654c81e2c0f9e9962d068ed79c364d6
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-02-12 22:02:30 +0330

    chore(prettier): remove default values in importOrder and add features directory

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,23 +1,15 @@
-/** @type {import("prettier").Config} */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
   trailingComma: "all",
-  semi: true,
   importOrder: [
-    "^@core/(.*)$",
-    "next/",
+    "^react$",
+    "^next/(.*)$",
     "<THIRD_PARTY_MODULES>",
     "^@server/(.*)$",
     "^@mui/(.*)$",
-    "@/components/",
+    "^@/features/(.*)$",
     "^[./]",
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  plugins: [
-    require.resolve(
-      "prettier-plugin-tailwindcss",
-    ),
-  ],
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
