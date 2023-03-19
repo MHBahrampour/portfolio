9684ce00c547d059f0e8efea3c97b0cbc7d5928d
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-03-20 01:04:19 +0330

    feat(prettier): add more config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,16 +1,26 @@
-module.exports = {
-  trailingComma: "all",
-  importOrder: [
-    "^react$",
-    "^next/(.*)$",
-    "<THIRD_PARTY_MODULES>",
-    "^@server/(.*)$",
-    "^@mui/(.*)$",
-    "^@/features/(.*)$",
-    "^@/utils/(.*)$",
-    "^[./]",
-  ],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+module.exports =
+  {
+    trailingComma:
+      "all",
+    printWidth: 80,
+    tabWidth: 2,
+    importOrder:
+      [
+        "^react$",
+        "^next/(.*)$",
+        "<THIRD_PARTY_MODULES>",
+        "^@server/(.*)$",
+        "^@mui/(.*)$",
+        "^@/features/(.*)$",
+        "^@/utils/(.*)$",
+        "^[./]",
+      ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins:
+      [
+        require.resolve(
+          "prettier-plugin-tailwindcss",
+        ),
+      ],
+  };
