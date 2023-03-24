f0bb9da58822d55734d43dc22226f46870fac664
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-03-24 16:59:37 +0330

    feat(footer): add more height

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -28,7 +28,7 @@ export default function Footer() {
   }, []);
 
   return (
-    <footer className="dpx h-40 mt-20 bg-[url('/images/footerBgLight.svg')] dark:bg-[url('/images/footerBgDark.svg')] bg-cover grid place-items-center">
+    <footer className="dpx h-48 mt-20 bg-[url('/images/footerBgLight.svg')] dark:bg-[url('/images/footerBgDark.svg')] bg-cover grid place-items-center">
       <Link href="https://github.com/MHBahrampour/portfolio" target="_blank">
         <div className="group grid gap-4 place-items-center p-4 mt-4 hover:scale-110 transition-all backdrop-blur-[2px] dark:backdrop-blur-[1px]">
           <Typography variant="body2" className="text-center">
