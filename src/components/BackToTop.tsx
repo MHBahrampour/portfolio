54f59b00d2465479e6586c9abfa77316dfa83c30
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-03-24 22:05:52 +0330

    fix(BackToTop): color of the button

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -15,7 +15,7 @@ export default function BackToTop() {
     <SmoothLink scrollToTop>
       <IconButton
         edge="end"
-        className={`grid place-items-center fixed right-4 md:right-6 rounded-full backdrop-blur-[2px] transition-all ${
+        className={`grid place-items-center fixed right-4 md:right-6 rounded-full backdrop-blur-[2px] transition-all text-current ${
           showTrigger ? "bottom-2 md:bottom-4" : "-bottom-10"
         }`}
       >
