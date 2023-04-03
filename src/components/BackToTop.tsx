394440f5e34ff555ccff488072a8884f6ccfb60a
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-04-03 18:07:22 +0330

    fix(BackToTop): update its position when it's not being showed

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -14,7 +14,7 @@ export default function BackToTop() {
     <SmoothLink scrollToTop>
       <IconButton
         className={`text-color-animation fixed right-4 z-10 grid place-items-center rounded-full backdrop-blur-md transition-all hover:scale-110 sm:right-[60px] sm:translate-x-1/2 sm:!bg-common-white dark:sm:!bg-common-black ${
-          showTrigger ? "bottom-4 sm:bottom-8" : "-bottom-10"
+          showTrigger ? "bottom-4 sm:bottom-8" : "-bottom-14"
         }`}
       >
         <TbSquareRoundedChevronLeftFilled className="rotate-90 text-3xl" />
